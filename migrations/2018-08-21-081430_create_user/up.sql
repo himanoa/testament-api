dfde25159354fbff69c0a604835dd77e0e3b2ef3
himanoa <matsunoappy@gmail.com>
2018-08-21 19:23:31 +0900

    feat: Add user table

---
+++ migrations/2018-08-21-081430_create_user/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE `users` (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  `uid` VARCHAR(256) NOT NULL
+);
--- src/schema.rs
+++ src/schema.rs
@@ -8,3 +8,16 @@ table! {
         published -> Bool,
     }
 }
+
+table! {
+    users (id) {
+        id -> Integer,
+        name -> Varchar,
+        uid -> Varchar,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    entries,
+    users,
+);
