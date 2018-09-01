5027516a37e434b048c410290fe62f79ab571b8e
himanoa <matsunoappy@gmail.com>
2018-09-01 20:48:49 +0900

    chor: Use dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+extern crate dotenv;
 extern crate testament;
 
 use std::env::var;
 use std::process::exit;
+
+use dotenv::dotenv;
+
 use testament::create_rocket;
 
 fn main() {
+    dotenv().ok();
     let database_url = match var("DATABASE_URL") {
         Ok(db) => db,
         Err(e) => {
