caaae4b4657d30f1c14efc5f489bf39eea02840d
himanoa <matsunoappy@gmail.com>
2018-09-27 02:59:39 +0900

    Fix compile

--- src/models/token.rs
+++ src/models/token.rs
@@ -1,5 +1,5 @@
 use chrono::NaiveDateTime;
-use diesel;
+use diesel::{insert_into, delete};
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use diesel::result::Error as DieselError;
@@ -29,11 +29,11 @@ pub struct NewToken {
 }
 
 pub trait TokenRepository {
-    fn resolve(&self, id: i32) -> Result<Token, DieselError>;
-    fn create(&self, user: User) -> Result<i32, DieselError>;
-    fn delete(&self, id: i32) -> Result<Token, DieselError>;
-    fn delete_all_by_user(&self, user: User) -> Result<i32, DieselError>;
-    fn list_by_user(&self, user: User) -> Result<Token, DieselError>;
+    fn resolve(&self, token: String) -> Result<Token, DieselError>;
+    fn create(&self, newToken: NewToken) -> Result<usize, DieselError>;
+    fn delete(&self, targetId: i32) -> Result<usize, DieselError>;
+    fn delete_all_by_user(&self, user: User) -> Result<usize, DieselError>;
+    fn list_by_user(&self, user: User) -> Result<Vec<Token>, DieselError>;
 }
 
 pub struct MySqlTokenRepository<'a> {
@@ -41,11 +41,39 @@ pub struct MySqlTokenRepository<'a> {
 }
 
 impl<'a> MySqlTokenRepository<'a> {
-    pub fn new(conn: &'a MySqlTokenRepository) -> MySqlTokenRepository {
+    pub fn new(conn: &'a MysqlConnection) -> MySqlTokenRepository {
         MySqlTokenRepository { conn: conn }
     }
 }
 
-impl<'a> Token for MySqlTokenRepository<'a> {
-
+impl<'a> TokenRepository for MySqlTokenRepository<'a> {
+    fn resolve(&self, input_token: String) -> Result<Token, DieselError> {
+        tokens.select((
+            tokens::id,
+            tokens::user_id,
+            tokens::token,
+            tokens::updated_at,
+            tokens::created_at,
+        )).filter(tokens::token.eq(input_token)).first::<Token>(self.conn)
+    }
+    fn create(&self, new_token: NewToken) -> Result<usize, DieselError> {
+        insert_into(tokens).values(new_token).execute(self.conn)
+    }
+    fn delete(&self, target_id: i32) -> Result<usize, DieselError> {
+        delete(tokens).filter(tokens::id.eq(target_id)).execute(self.conn)
+    }
+    fn delete_all_by_user(&self, user: User) -> Result<usize, DieselError> {
+        delete(tokens).filter(tokens::user_id.eq(user.id)).execute(self.conn)
+    }
+    fn list_by_user(&self, user: User) -> Result<Vec<Token>, DieselError> {
+        tokens
+            .select((
+                tokens::id,
+                tokens::user_id,
+                tokens::token,
+                tokens::updated_at,
+                tokens::created_at
+            ))
+            .filter(tokens::user_id.eq(user.id)).get_results::<Token>(self.conn)
+    }
 }
