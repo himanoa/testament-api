d61b7b2601d57f69aa03b19a20d251ba5c7a6ced
himanoa <matsunoappy@gmail.com>
2018-09-27 01:28:46 +0900

    Implement token repository

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod auth;
 pub mod entry;
 pub mod user;
+pub mod token;
--- src/models/token.rs
+++ src/models/token.rs
@@ -7,11 +7,12 @@ use infra::mysql::DbConn;
 use schema::users;
 use schema::tokens;
 use schema::tokens::dsl::*;
+use models::user::User;
 
-#[derive(Debgu, Serialize, Deserialize, Queryable, Identifiable)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Associations)]
 #[table_name = "tokens"]
 #[belongs_to(User)]
-pub struct Tokens {
+pub struct Token {
     pub id: i32,
     pub user_id: i32,
     pub token: String,
@@ -19,11 +20,32 @@ pub struct Tokens {
     pub updated_at: NaiveDateTime
 }
 
-#[derive(Debgu, Serialize, Deserialize, Insertable)]
+#[derive(Debug, Serialize, Deserialize, Insertable, Associations)]
 #[table_name = "tokens"]
 #[belongs_to(User)]
-pub struct NewTokens {
+pub struct NewToken {
     pub user_id: i32,
     pub token: String,
 }
 
+pub trait TokenRepository {
+    fn resolve(&self, id: i32) -> Result<Token, DieselError>;
+    fn create(&self, user: User) -> Result<i32, DieselError>;
+    fn delete(&self, id: i32) -> Result<Token, DieselError>;
+    fn delete_all_by_user(&self, user: User) -> Result<i32, DieselError>;
+    fn list_by_user(&self, user: User) -> Result<Token, DieselError>;
+}
+
+pub struct MySqlTokenRepository<'a> {
+    conn: &'a MysqlConnection,
+}
+
+impl<'a> MySqlTokenRepository<'a> {
+    pub fn new(conn: &'a MySqlTokenRepository) -> MySqlTokenRepository {
+        MySqlTokenRepository { conn: conn }
+    }
+}
+
+impl<'a> Token for MySqlTokenRepository<'a> {
+
+}
